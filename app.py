686416be70badd3267dccd3092504828b7d5b022
James Shapiro <jamesshapiro@gmail.com>
2022-01-22 00:06:38 -0500

    add create reminder function

--- app.py
+++ app.py
@@ -3,11 +3,11 @@ import os
 
 import aws_cdk as cdk
 
-from cdk_reminders.cdk_reminders_stack import CdkRemindersStack
+from cdk_reminders.cdk_reminders_stack import CdkRemindersAppStack
 
 
 app = cdk.App()
-CdkRemindersStack(app, "CdkRemindersStack",
+CdkRemindersAppStack(app, "CdkRemindersAppStack",
     # If you don't specify 'env', this stack will be environment-agnostic.
     # Account/Region-dependent features and context lookups will not work,
     # but a single synthesized template can be deployed anywhere.
--- cdk_reminders/cdk_reminders_stack.py
+++ cdk_reminders/cdk_reminders_stack.py
@@ -1,19 +1,61 @@
 from aws_cdk import (
-    # Duration,
     Stack,
+        # Duration,
+    aws_lambda as lambda_,
+    aws_dynamodb as dynamodb,
+    aws_apigateway as apigateway,
+    # aws_lambda_python_alpha as lambda_python
     # aws_sqs as sqs,
 )
+import aws_cdk as cdk
+import os
+
 from constructs import Construct
 
-class CdkRemindersStack(Stack):
+class CdkRemindersAppStack(Stack):
 
     def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
 
-        # The code that defines your stack goes here
+        ddb_table = dynamodb.Table(
+            self, "Table",
+            partition_key=dynamodb.Attribute(name="PK1", type=dynamodb.AttributeType.STRING),
+            sort_key=dynamodb.Attribute(name="SK1", type=dynamodb.AttributeType.STRING),
+            billing_mode=dynamodb.BillingMode.PAY_PER_REQUEST
+        )
 
-        # example resource
-        # queue = sqs.Queue(
-        #     self, "CdkRemindersQueue",
-        #     visibility_timeout=Duration.seconds(300),
-        # )
+        ulid_layer = lambda_.LayerVersion(
+            self,
+            "Ulid38Layer",
+            removal_policy=cdk.RemovalPolicy.DESTROY,
+            code=lambda_.Code.from_asset('resources/my-Python38-ulid.zip'),
+            compatible_architectures=[lambda_.Architecture.X86_64]
+        )
+
+        reminder_creator_function_cdk = lambda_.Function(
+            self, "ReminderCreatorCDK",
+            runtime=lambda_.Runtime.PYTHON_3_8,
+            code=lambda_.Code.from_asset("resources"),
+            handler="create_reminder.lambda_handler",
+            environment=dict(
+                REMINDERS_DDB_TABLE=ddb_table.table_name
+            ),
+            layers=[ulid_layer]
+        )
+
+        api = apigateway.RestApi(
+            self,
+            "reminders-api-cdk",
+            rest_api_name="reminders-api-cdk",
+            description="Reminder service in CDK."
+        )
+
+        create_reminder_integration = apigateway.LambdaIntegration(
+            reminder_creator_function_cdk,
+            request_templates={"application/json": '{ "statusCode": "200" }'}
+        )
+
+        api.root.add_method("POST", create_reminder_integration)
+
+        ddb_table.grant_write_data(reminder_creator_function_cdk)
+
