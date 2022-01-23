c296716cab6c7cc10c024e26873796774da7ff16
James Shapiro <jamesshapiro@gmail.com>
2022-01-22 19:24:04 -0500

    create core functionality for reminders app in CDK

--- cdk_reminders/cdk_reminders_stack.py
+++ cdk_reminders/cdk_reminders_stack.py
@@ -4,6 +4,11 @@ from aws_cdk import (
     aws_lambda as lambda_,
     aws_dynamodb as dynamodb,
     aws_apigateway as apigateway,
+    aws_events as events,
+    aws_events_targets as targets,
+    aws_sns as sns,
+    aws_sns_subscriptions as subscriptions,
+    aws_iam as iam,
     # aws_lambda_python_alpha as lambda_python
     # aws_sqs as sqs,
 )
@@ -11,6 +16,9 @@ import aws_cdk as cdk
 import os
 
 from constructs import Construct
+
+phone_number = os.environ['NotificationPhone']
+email = os.environ['NotificationEmail']
 
 class CdkRemindersAppStack(Stack):
 
@@ -28,7 +36,7 @@ class CdkRemindersAppStack(Stack):
             self,
             "Ulid38Layer",
             removal_policy=cdk.RemovalPolicy.DESTROY,
-            code=lambda_.Code.from_asset('resources/my-Python38-ulid.zip'),
+            code=lambda_.Code.from_asset('layers/my-Python38-ulid.zip'),
             compatible_architectures=[lambda_.Architecture.X86_64]
         )
 
@@ -40,6 +48,27 @@ class CdkRemindersAppStack(Stack):
             environment=dict(
                 REMINDERS_DDB_TABLE=ddb_table.table_name
             ),
+            timeout=cdk.Duration.seconds(30),
+            memory_size=128,
+            layers=[ulid_layer]
+        )
+
+        topic = sns.Topic(self, "ReminderCDKTopic")
+
+        reminder_sender_function_cdk = lambda_.Function(
+            self, "ReminderSenderCDK",
+            runtime=lambda_.Runtime.PYTHON_3_8,
+            code=lambda_.Code.from_asset("resources"),
+            handler="send_reminders.lambda_handler",
+            environment=dict(
+                REMINDERS_DDB_TABLE=ddb_table.table_name,
+                REMINDERS_TOPIC = topic.topic_arn,
+                # TODO: turn this into a formal parameter
+                REMINDERS_PHONE_NUMBER = phone_number
+                # TODO: turn this into a formal parameter
+            ),
+            timeout=cdk.Duration.seconds(30),
+            memory_size=128,
             layers=[ulid_layer]
         )
 
@@ -55,7 +84,20 @@ class CdkRemindersAppStack(Stack):
             request_templates={"application/json": '{ "statusCode": "200" }'}
         )
 
+        lambda_target = targets.LambdaFunction(reminder_sender_function_cdk)
+
+        cron_rule = events.Rule(self, "ScheduleRule",
+            schedule=events.Schedule.cron(minute="*", hour="*"),
+            targets=[lambda_target]
+        )
+
+        topic.grant_publish(reminder_sender_function_cdk)
+        email_address = email
+        #my_topic.add_subscription(subscriptions.EmailSubscription(email_address.value_as_string))
+        topic.add_subscription(subscriptions.EmailSubscription(email_address))
+        reminder_sender_function_cdk.role.add_managed_policy(iam.ManagedPolicy.from_aws_managed_policy_name("AmazonSNSFullAccess"))
+
         api.root.add_method("POST", create_reminder_integration)
 
         ddb_table.grant_write_data(reminder_creator_function_cdk)
-
+        ddb_table.grant_read_write_data(reminder_sender_function_cdk)
