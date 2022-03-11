ae1d6203b1364dfd639880389aabaada525aeeb0
James Shapiro <jamesshapiro@gmail.com>
2022-03-11 03:15:11 -0500

    add template for private, password protected website to CDK

--- cdk_reminders/cdk_reminders_stack.py
+++ cdk_reminders/cdk_reminders_stack.py
@@ -10,6 +10,13 @@ from aws_cdk import (
     aws_iam as iam,
     custom_resources as custom_resources,
     CfnParameter,
+    aws_certificatemanager as certificatemanager,
+    aws_cloudfront as cloudfront,
+    aws_cloudfront_origins as origins,
+    aws_route53 as route53,
+    aws_route53_targets as route53_targets,
+    Aws, CfnOutput, Duration,
+    aws_s3 as s3,
 )
 import aws_cdk as cdk
 
@@ -19,22 +26,23 @@ class CdkRemindersAppStack(Stack):
 
     def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
-        # with open(".cdk-params") as f:
-        #     lines = f.read().splitlines()
-        #     # .cdk-params should be of the form (note the required country code in phone #):
-        #     # NotificationPhone=+12223334444
-        #     # NotificationEmail=jeff@example.com
-        #     phone_number = [line for line in lines if line.startswith('NotificationPhone')][0].split('=')[1]
-        #     email = [line for line in lines if line.startswith('NotificationEmail')][0].split('=')[1]
-        phone_number = CfnParameter(self, 'NotificationPhone', 
-            description="Phone number to receive reminders. For US numbers, should be of the form +12223334444 with +1 as the country code.")
-        email = CfnParameter(self,'NotificationEmail', description="Email to receive reminders.")
+        with open(".cdk-params") as f:
+            lines = f.read().splitlines()
+            # .cdk-params should be of the form: key_name=value
+            subdomain = [line for line in lines if line.startswith('subdomain=')][0].split('=')[1]
+            hosted_zone_id = [line for line in lines if line.startswith('hosted_zone_id=')][0].split('=')[1]
+            phone_number = [line for line in lines if line.startswith('NotificationPhone')][0].split('=')[1]
+            email = [line for line in lines if line.startswith('NotificationEmail')][0].split('=')[1]
+        #phone_number = CfnParameter(self, 'NotificationPhone', 
+        #    description="Phone number to receive reminders. For US numbers, should be of the form +12223334444 with +1 as the country code.")
+        #email = CfnParameter(self,'NotificationEmail', description="Email to receive reminders.")
         ddb_table = dynamodb.Table(
             self, "Table",
             partition_key=dynamodb.Attribute(name="PK1", type=dynamodb.AttributeType.STRING),
             sort_key=dynamodb.Attribute(name="SK1", type=dynamodb.AttributeType.STRING),
             billing_mode=dynamodb.BillingMode.PAY_PER_REQUEST
         )
+        CfnOutput(self, "DDBTableName", value=ddb_table.table_name)
 
         ulid_layer = lambda_.LayerVersion(
             self,
@@ -52,7 +60,7 @@ class CdkRemindersAppStack(Stack):
             environment=dict(
                 REMINDERS_DDB_TABLE=ddb_table.table_name
             ),
-            timeout=cdk.Duration.seconds(30),
+            timeout=Duration.seconds(30),
             memory_size=128,
             layers=[ulid_layer]
         )
@@ -67,9 +75,9 @@ class CdkRemindersAppStack(Stack):
             environment={
                 "REMINDERS_DDB_TABLE": ddb_table.table_name,
                 "REMINDERS_TOPIC": topic.topic_arn,
-                "REMINDERS_PHONE_NUMBER": phone_number.value_as_string
+                "REMINDERS_PHONE_NUMBER": phone_number
             },
-            timeout=cdk.Duration.seconds(30),
+            timeout=Duration.seconds(30),
             memory_size=128,
             layers=[ulid_layer]
         )
@@ -94,7 +102,7 @@ class CdkRemindersAppStack(Stack):
 
         topic.grant_publish(reminder_sender_function_cdk)
         email_address = email
-        topic.add_subscription(subscriptions.EmailSubscription(email_address.value_as_string))
+        topic.add_subscription(subscriptions.EmailSubscription(email_address))
         reminder_sender_function_cdk.role.add_managed_policy(iam.ManagedPolicy.from_aws_managed_policy_name("AmazonSNSFullAccess"))
 
         api.root.add_method("POST", create_reminder_integration, api_key_required=True)
@@ -120,7 +128,7 @@ class CdkRemindersAppStack(Stack):
                 environment=dict(
                     API_KEY_ID=api_key.key_id,
                 ),
-                timeout=cdk.Duration.seconds(30),
+                timeout=Duration.seconds(30),
                 memory_size=128
             )
 
@@ -149,5 +157,105 @@ class CdkRemindersAppStack(Stack):
         cdk.CfnOutput(
             self, "ReminderApi",
             description="Reminder Api",
-            value = f'https://{api.rest_api_id}.execute-api.us-east-1.amazonaws.com/prod/'
+            value = f'https://{api.rest_api_id}.execute-api.{Aws.REGION}.amazonaws.com/prod/'
         )
+
+        ######## FRONT-END PRIVATE WEBSITE ########
+        zone = route53.HostedZone.from_hosted_zone_attributes(self, "HostedZone",
+            hosted_zone_id=hosted_zone_id,
+            zone_name=subdomain
+        )
+
+        site_bucket = s3.Bucket(
+            self, f'{subdomain}-bucket',
+        )
+        certificate = certificatemanager.DnsValidatedCertificate(
+            self, f'{subdomain}-certificate',
+            domain_name=subdomain,
+            hosted_zone=zone,
+            subject_alternative_names=[f'www.{subdomain}']
+        )
+
+        # authorizer_lambda_policy = iam.PolicyStatement(
+        #     self, 'root',
+        #     statements=[
+        #         iam.PolicyStatement(
+        #             actions=['iam:GetRole*'],
+        #             resources=[f'arn:aws:iam::{Aws.ACCOUNT_ID}:role/{Aws.STACK_NAME}-AuthorizerFnServiceRole*']
+        #         ),
+        #         # TODO: limit PassRole to the ec2 role
+        #         iam.PolicyStatement(
+        #             actions=['dynamodb:GetItem'],
+        #             resources=[ddb_table.table_arn]
+        #         ),
+                
+        #     ]
+        # )
+
+        
+        
+        domain_names = [subdomain, f'www.{subdomain}']
+        authorizer_function = cloudfront.experimental.EdgeFunction(self, "Authorizer",
+            runtime=lambda_.Runtime.PYTHON_3_9,
+            code=lambda_.Code.from_asset('lambda_edge'),
+            handler='authorizer.lambda_handler',
+        )
+        statement_1 = iam.PolicyStatement(
+            actions=['iam:GetRole*','iam:ListRolePolicies'],
+            resources=[f'arn:aws:iam::{Aws.ACCOUNT_ID}:role/{Aws.STACK_NAME}-AuthorizerFnServiceRole*']
+        )
+        statement_2 = iam.PolicyStatement(
+            actions=['dynamodb:GetItem'],
+            resources=[ddb_table.table_arn]
+        )
+
+        #authorizer_function.role.attach_inline_policy(authorizer_lambda_policy)
+        authorizer_function.add_to_role_policy(statement_1)
+        authorizer_function.add_to_role_policy(statement_2)
+        
+        #     DistributionAuthorizerRole:
+        #   Policies:
+        #     - PolicyName: root
+        #         Statement:
+        #           - Effect: Allow
+        #             Action: iam:GetRole*
+        #             Resource: !Sub arn:aws:iam::${AWS::AccountId}:role/${AWS::StackName}-DistributionAuthorizerRole-*
+
+        
+        distribution = cloudfront.Distribution(
+            self, f'{subdomain}-distribution',
+            default_behavior=cloudfront.BehaviorOptions(
+                origin=origins.S3Origin(site_bucket),
+                allowed_methods=cloudfront.AllowedMethods.ALLOW_ALL,
+                viewer_protocol_policy=cloudfront.ViewerProtocolPolicy.REDIRECT_TO_HTTPS,
+                edge_lambdas=[
+                    cloudfront.EdgeLambda(
+                        function_version=authorizer_function.current_version,
+                        event_type=cloudfront.LambdaEdgeEventType.VIEWER_REQUEST
+                    )
+                ]
+            ),
+            error_responses=[
+                cloudfront.ErrorResponse(
+                    http_status=403,
+                    response_http_status=200,
+                    response_page_path="/index.html",
+                    ttl=Duration.minutes(30)
+                )
+            ],
+            comment=f'{subdomain} S3 HTTPS',
+            default_root_object='index.html',
+            domain_names=domain_names,
+            certificate=certificate
+        )
+
+        CfnOutput(self, f'{subdomain}-cf-distribution', value=distribution.distribution_id)
+        a_record_target = route53.RecordTarget.from_alias(route53_targets.CloudFrontTarget(distribution))
+        route53.ARecord(
+            self, f'{subdomain}-alias-record',
+            zone=zone,
+            target=a_record_target,
+            record_name=subdomain
+        )
+        CfnOutput(self, f'{subdomain}-bucket-name', value=site_bucket.bucket_name)
+        
